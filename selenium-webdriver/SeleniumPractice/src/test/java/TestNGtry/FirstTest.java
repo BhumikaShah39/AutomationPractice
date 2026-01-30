28631086e212a55292dcf080560711ff3da58d82
Quality assurance <quality.a@guardware.com.au>
2026-01-30 16:22:34 +0545

    integrated allure reports

--- selenium-webdriver/SeleniumPractice/src/test/java/TestNGtry/FirstTest.java
+++ selenium-webdriver/SeleniumPractice/src/test/java/TestNGtry/FirstTest.java
@@ -1,5 +1,6 @@
 package TestNGtry;
 
+import io.qameta.allure.SeverityLevel;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,49 +12,41 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import io.qameta.allure.*;
 
 
+@Epic("PDF Tools")
+@Feature("PDF to Word")
 public class FirstTest {
-    //global var
+
     WebDriver driver;
     WebDriverWait wait;
+
     @Test(priority = 1)
-    void openApp(){
+    @Story("Open application")
+    @Description("Launch iLovePDF website")
+    void openApp() {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://www.ilovepdf.com/");
-        System.out.println("Opened ilovepdf");
     }
 
     @Test(priority = 2)
-    void verifyAndMove(){
-        boolean status = driver.findElement(By.xpath("//img[@alt='iLovePDF']")).isDisplayed();
-        if(status)
-        {
-            System.out.println("Logo is present");
-        }
+    @Story("Navigate to PDF to Word")
+    void verifyAndMove() {
         driver.findElement(By.xpath("//a[@title='PDF to Word']")).click();
-        boolean pdfPage = driver.findElement(By.xpath("//h1[normalize-space()='PDF to WORD Converter']")).isDisplayed();
-        if(pdfPage)
-        {
-            System.out.println("Moved Forward to :PDF TO WORD");
-        }
     }
 
-
-    @Test(priority = 3, dependsOnMethods = {"verifyAndMove"})
+    @Test(priority = 3, dependsOnMethods = "verifyAndMove")
+    @Story("Upload PDF and convert")
+    @Severity(SeverityLevel.CRITICAL)
     void uploadFile() {
-        String file1 = "C:\\Users\\Qualityassurance\\OneDrive - guardware.com.au\\Desktop\\acdas.pdf";
+        String file1 = "C:\\Users\\Qualityassurance\\OneDrive - guardware.com.au\\Desktop\\test.pdf";
         WebElement fileInput = wait.until(
                 ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='file']"))
         );
         fileInput.sendKeys(file1);
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("merge-options")));
-        driver.findElement(By.id("processTask")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[contains(text(),'Your PDF has been converted')]")));
-
-        System.out.println("Word (.docx) file is ready for download");
     }
 }
+
